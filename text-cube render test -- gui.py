110a8faf404254b1efc5e2e6569be8d99e572849
Minoxs <gufrgs2018@gmail.com>
2019-08-11 03:10:30 -0300

    Tests done -- switching to Linux

--- text-cube render test -- gui.py
+++ text-cube render test -- gui.py
@@ -1,26 +1,26 @@
-a = [0,0,0,0]
-b = [0,0,0,0]
-c = [0,0,0,0]
-d = [0,0,0,0]
-e = [0,0,0,0]
-f = [0,0,0,0]
-g = [0,0,0,0]
-h = [0,0,0,0]
-i = [0,0,0,0]
-j = [0,0,0,0]
-k = [0,0,0,0]
-l = [0,0,0,0]
+size_start = input("Cube Size: ")
+size = int(size_start)
+def create_row(size):
+	row = [0 for i in range(size)]
+	return row
+
+def row_nums(size):
+	row_n = [i for i in range(size)]
+	a = zip(row_n,row_n)
+	return a
+
+def dict(size):
+	lst = row_nums(size)
+	rows = {row:wor for row, wor in lst}
+	return rows
+
+a = dict(size)
+for i in range(size):
+	a[i] = create_row(size)	
+
+class Row:
+	def row(self,size):
+		row = [0 for i in range(size)]
+		return row
 
 
-def call(pos1, i, pos2, j, pos3, k):
-	a[pos1-1] = i
-	b[pos2-1] = j
-	c[pos3-1] = k
-
-def render():
-	print(a,b,c)
-	print(d,e,f)
-	print(g,h,i)
-	print(j,k,l)
-
-print("Testing what we doin")
