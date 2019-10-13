29335cbca5479b7cfec20f7f2253c1e917c91388
Minoxs <gufrgs2018@gmail.com>
2019-10-12 23:46:08 -0300

    basically done

--- cube.py
+++ cube.py
@@ -1,16 +1,18 @@
 ﻿#Initial Setup and Useful Info
 import random
-primes_list = [2,3,5,7,11,13]
-cube_colours = ['White','Red','Green','Orange','Blue','Yellow']
-mix = zip(primes_list,cube_colours)
-colour_prime_relation = list(mix)
-msg = 'Cube piece is '
-print("The following list is the number and its associated colour:")
-print(colour_prime_relation)
-######################################################################
-size_start = input("Cube Size: ")
-size = int(size_start)
-############################################## Nem me pergunte como isso funciona
+size = 0
+while size < 1:
+	size_start = input("Cube Size: ")
+	size = int(size_start)
+############################################## 
+#Special Size Case
+if size == 1:
+	print("Cute little cube!")
+	print("Already finished, congratulations :)")
+	hold = input("ENTER to close.")
+	exit()
+##############################################
+# Nem me pergunte como isso funciona
 # Essa parte é encarregada de gerar o 'esqueleto' do cubo
 def create_row(size):
 	row = [0 for i in range(size)]
@@ -30,10 +32,6 @@ row = dict(size)
 for i in range(size):
 	row[i] = create_row(size**2)	
 ###############################################
-
-def c(i,j,to): # Modifica um valor na posição (i,j) para 'to'
-	row[i-1][j-1] = to 
-
 def fh(choice): # Rotaciona a Face número 'choice' no sentido horário
 	choice = choice - 1
 	if choice < 0:
@@ -82,7 +80,7 @@ def fa(choice): # Rotaciona a Face número 'choice' no sentido anti-horário
 		j += -1
 	render()
 
-def td(choice): #Mexe a linha 'choice' para a direita
+def ld(choice): #Mexe a linha 'choice' para a direita
 	choice = choice - 1
 	if choice < 0:
 		print("Invalid Choice.")
@@ -94,7 +92,7 @@ def td(choice): #Mexe a linha 'choice' para a direita
 	row[choice][:size] = temp
 	render()
 
-def te(choice): #Mexe a linha 'choice' para a esquerda
+def le(choice): #Mexe a linha 'choice' para a esquerda
 	choice = choice - 1
 	if choice < 0:
 		print("Invalid Choice.")
@@ -106,7 +104,7 @@ def te(choice): #Mexe a linha 'choice' para a esquerda
 	row[choice][-size:] = temp
 	render()
 
-def rh(choice): #Rotate a column down -> up (clockwise)
+def cc(choice): #Rotate a column down -> up (clockwise)
 	choice = choice - 1
 	if choice < 0:
 		print("Invalid Choice.")
@@ -127,7 +125,7 @@ def rh(choice): #Rotate a column down -> up (clockwise)
 			k += 1
 	render()
 
-def rc(choice): #Rotate a column up -> down (counter-clockwise)
+def cb(choice): #Rotate a column up -> down (counter-clockwise)
 	choice = choice - 1
 	if choice < 0:
 		print("Invalid Choice.")
@@ -159,155 +157,6 @@ def render(size = size): #Renderiza os 'slices' do cubo separadamente
 		line += " |"
 		print(line)
 ####################################################################################
-
-# Como esse cubo funciona: Cada cor tem um número primo associado, peças de
-# multiplas cores é apenas o produto dos números primos associado às cores da
-# peça. A lista se diz 'primal' quando contém apenas os números associados.
-# A lista se diz 'legível' quando os números foram transformados nas suas
-# respectivas cores.
-
-#Função que detecta cor da peça
-def get_colour(cube_piece):
-	piece_colour = ' '
-	if cube_piece%2 == 0:
-		piece_colour += 'White, '
-	if cube_piece%3 == 0:
-		piece_colour += 'Red, '
-	if cube_piece%5 == 0:
-		piece_colour += 'Green, '
-	if cube_piece%7 == 0:
-		piece_colour += 'Orange, '
-	if cube_piece%11 == 0:
-		piece_colour += 'Blue, '
-	if cube_piece%13 == 0:
-		piece_colour += 'Yellow, '
-	piece_colour_final = piece_colour[1:-2]+'.'
-	final = msg + piece_colour_final
-	return final
-
-#Mesma utilidade que get_colour, porém retorna apenas a letra inicial da cor
-def short(cube_piece):
-	piece_colour = ' '
-	if cube_piece%2 == 0:
-		piece_colour += 'W'
-	if cube_piece%3 == 0:
-		piece_colour += 'R'
-	if cube_piece%5 == 0:
-		piece_colour += 'G'
-	if cube_piece%7 == 0:
-		piece_colour += 'O'
-	if cube_piece%11 == 0:
-		piece_colour += 'B'
-	if cube_piece%13 == 0:
-		piece_colour += 'Y'
-	piece_colour_final = piece_colour[1:]
-	final = piece_colour_final
-	return final
-
-#Função que gera as peças centrais (de 1 cor) para um cubo de tamanho qualquer
-def gen_centres(cube_size = 3):
-	if cube_size <= 1:
-		return "Error"
-	primes_list = [2,3,5,7,11,13]
-	centre_size = cube_size - 2
-	centres = primes_list*centre_size**2
-	centres.sort()
-	return centres
-
-#Função que gera as peças do meio (de 2 cores) para um cubo de tamanho qualquer
-def gen_edges(cube_size = 3):
-	if cube_size <= 1:
-		return "Error"
-	primes_list = [2,3,5,7,11,13]
-	centres = primes_list
-	edge_pieces = []
-	size = cube_size-2
-	for i in centres:
-		var = i
-		mult = list(range(len(centres)))
-		mult_slice = mult[centres.index(i):]
-		for h in mult_slice:
-			var2 = centres[h]
-			if var == var2:
-				continue
-			else:
-				piece = var*var2
-				edge_pieces.append(piece)
-	final = edge_pieces*size
-	return final
-
-#Função que gera as peças do canto (de 3 cores) para um cubo de tamanho qualquer
-def gen_corners(cube_size = 3):
-	if cube_size <= 1:
-		return "Error"
-	primes_list = [2,3,5,7,11,13]
-	centres = primes_list
-	edges = gen_edges()
-	corner_pieces = []
-	for i in edges:
-		var = i
-		for h in primes_list:
-			var2 = h
-			inx = edges.index(i)
-			inx2 = primes_list.index(h)
-			if var == var2:
-				continue
-			if edges[inx]%primes_list[inx2]==0:
-				continue
-			piece = var*var2
-			if piece in corner_pieces:
-				continue
-			corner_pieces.append(piece)
-	return corner_pieces
-
-#As funções passadas criam algumas peças que não existem (ex: branca e amarela)
-#Essa função remove tais peças
-def remove_impossible_pieces(func, cube_size = 3):
-	lst = func
-	new_list = []
-	for i in lst:
-		if i%5==0 and i%11==0:
-			continue
-		if i%2==0 and i%13==0:
-			continue
-		if i%3==0 and i%7==0:
-			continue
-		new_list.append(i)
-	return new_list
-
-#Função de teste -- diz a cor das peças sem transformar a lista de primal para legível
-def check_colours(cube):
-	for i in cube:
-		a = short(i)
-		print(a)
-
-#Transforma a lista de peças da forma "primal" para forma legível -- não tem como reverter (função de reverter pode ser feita caso necessário)
-def transform(cube):
-	empty_list = []
-	for i in cube:
-		a = short(i)
-		empty_list.append(a)
-	return empty_list
-
-#Função que junta as anteriores para gerar o cubo -- por enquanto gera uma lista unidimensional das peças
-def gen_all_pieces(cube_size = 3):
-	if cube_size <= 1:
-		print("Cube Size Error")
-		return "Cube Size Error"
-	centres = gen_centres(cube_size)
-	edge_pieces = remove_impossible_pieces(gen_edges(cube_size))
-	corner_pieces = remove_impossible_pieces(gen_corners(cube_size))
-	centres.sort()
-	edge_pieces.sort()
-	corner_pieces.sort()
-	f_centre = transform(centres)
-	f_edge = transform(edge_pieces)
-	f_corner = transform(corner_pieces)
-	final = f_centre,f_edge,f_corner
-	return final
-
-all_pieces = gen_all_pieces(size)
-print(all_pieces)
 ##########CONSTRUINDO O CUBO##########
 #
 l = size-1
@@ -330,20 +179,85 @@ for i in range(size+1,size*2):
 	row[l][i] = "_W_"
 row[0][size*2-1] = "Y_G"
 row[1][size*2-1] = "_G_"
+row[l][size*2-1] = "W_G"
 row[0][size**2-size] = "OYB"
 row[1][size**2-size] = "O_B"
+row[l][size**2-size] = "OWB"
 for i in range(size**2-size+1,size**2):
 	row[0][i] = "O_Y"
 	row[1][i] = "_O_"
+	row[l][i] = "O_W"
 row[0][size**2-1] = "OYG"
 row[1][size**2-1] = "O_G"
+row[l][size**2-1] = "OWG"
 row[0][size*2:size**2-size] = row[0][size:size*2]*(size-3)
 row[1][size*2:size**2-size] = row[1][size:size*2]*(size-3)
+row[l][size*2:size**2-size] = row[l][size:size*2]*(size-3)
 for i in range(2,size-1):
 	row[i] = row[1]
 row[size-1][size*2:size**2-size] = row[size-1][size:size*2]*(size-3)
 #
-
 render()
-
-a = input("Press ENTER to exit.")
+###########################################################################
+# Scrambling Cube
+hold = input("Starting Scramble...")
+moves = []
+move_row = []
+for i in range(size**3):
+	moves.append(random.randint(1,6))
+	move_row.append(random.randint(1,size))
+for i in range(moves):
+	if moves[i] == 1:
+		fh(move_row[i])
+	if moves[i] == 2:
+		fc(move_row[i])
+	if moves[i] == 3:
+		le(move_row[i])
+	if moves[i] == 4:
+		ld(move_row[i])
+	if moves[i] == 5:
+		cc(move_row[i])
+	if moves[i] == 6:
+		cb(move_row[i])
+hold = input("Scramble Finished!")
+#
+###########################################################################
+# Taking Commands
+command = 0
+while command != "exit":
+	com = input("Choose a move.")
+	try:
+		command = com.lower()
+	except:
+		continue
+	if command == "r":
+		cc(size)
+	elif command == "r'":
+		cb(size)
+	elif command == "l":
+		cb(1)
+	elif command == "l'":
+		cc(1)
+	elif command == "b":
+		fa(size)
+	elif command == "b'":
+		fh(size)
+	elif command == "d":
+		ld(size)
+	elif command == "d'":
+		le(size)
+	elif command == "f":
+		fh(1)
+	elif command == "f'":
+		fa(1)
+	elif command == "u":
+		le(1)
+	elif command == "u'":
+		ld(1)
+	else:
+		try:
+			eval(command)
+		except:
+			print("Invalid Command.")
+			continue
+###########################################################################
