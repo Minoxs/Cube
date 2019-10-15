d3a4d13792e22026814ff54546a45f04bdd01f03
Minoxs <gufrgs2018@gmail.com>
2019-10-15 14:11:44 -0300

    big changes?

--- cube.py
+++ cube.py
@@ -1,68 +1,4 @@
-﻿#Initial Setup and Useful Info
-lb = "\n"
-import random
-import platform
-import datetime
-from pathlib import Path as find
-error_msg = "Something went Wrong!"
-
-#Função que detecta e diz o sistema quando compilado
-def os_detect():
-	sys = platform.system()
-	plt = platform.release()
-	os_name = "{} {}".format(sys,plt)
-	print("Sistema Operacional: "+os_name)
-
-os_detect()
-
-#Função que define aonde será salvo logs
-def set_filepath(save_to = "not_set"):
-	saving = str(find.cwd())
-	return saving
-
-filepath = set_filepath()
-print("Pasta atual: "+filepath)
-
-time_now = datetime.datetime.now()
-print("Data: "+str(time_now))
-
-############################################################################################
-#Basic Functions############################################################################
-#Adiciona o que acontece em um .txt
-
-def printer(add_to_log, title = "log", mode = "a+", extension = ".txt"):
-	path = "{}{}{}".format(filepath,title,extension)
-	p = open(path, mode)
-	p.write(str(add_to_log) + lb)
-	p.close()
-############################################################################################
-###############################################
-# Main Menu
-def menu():
-	menu = 1
-	print("""
-		#====================#\n
-		#  1 - Start Game	 #\n
-		#  2 - Options	     #\n
-		#  3 - Instructions  #\n
-		#  4 - Exit 		 #\n
-		#====================#\n
-		""")
-	while menu == 1:
-		inp = input("What do:")
-		try:
-			inp = int(inp)
-		except:
-			print("Invalid Command!")
-			continue
-		if inp == 1:
-			break
-
-menu()
-###############################################
-###############################################
-# Settings
-
+﻿import random
 ###############################################
 size = 0
 while size < 1:
@@ -332,8 +268,9 @@ render()
 #
 ###########################################################################
 # Taking Commands
+command_enable = 1
 command = 0
-while command != "exit":
+while command_enable == 1:
 	com = input("Choose a move: ")
 	try:
 		command = com.lower()
@@ -392,6 +329,8 @@ while command != "exit":
 		else:
 			print("Keep trying!")
 			print(mistakes)
+	elif command == "exit":
+		import menu
 	else:
 		try:
 			eval(command)
